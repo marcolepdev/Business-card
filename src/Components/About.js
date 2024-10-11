64825ef866e9601e96e27c5d351313ca7aac3cea
MarcoLepDev <88764817+marcolepdev@users.noreply.github.com>
2024-10-11 15:17:26 +0200

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,7 +5,7 @@ function About() {
     <div className="about">
       <h1 id="about-title">About</h1>
       <p id="about-par">
-        I love for languages,programming, and design. Combining creativity with technical skills, I
+        I love languages, programming and design. By combining creativity with technical skills, I
         enjoy building user-friendly web applications that offer both
         functionality and aesthetics.
       </p>
